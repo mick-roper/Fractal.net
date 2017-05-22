7a9a744a7631d990a75aa627daaf2ffd658d130c
mick-roper <mick@theropers.co.uk>
2017-05-22 15:59:55 +0100

    Added a simple mandelbrot calculator

--- Src/Fractals.Forms/MainForm.Designer.cs
+++ Src/Fractals.Forms/MainForm.Designer.cs
@@ -30,27 +30,52 @@
         {
             this.canvas = new System.Windows.Forms.PictureBox();
             this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.goToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             ((System.ComponentModel.ISupportInitialize)(this.canvas)).BeginInit();
+            this.menuStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
             // canvas
             // 
-            this.canvas.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.canvas.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.canvas.BackColor = System.Drawing.SystemColors.Control;
+            this.canvas.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.canvas.Location = new System.Drawing.Point(0, 24);
             this.canvas.Name = "canvas";
             this.canvas.Size = new System.Drawing.Size(784, 537);
+            this.canvas.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
             this.canvas.TabIndex = 0;
             this.canvas.TabStop = false;
             // 
             // menuStrip1
             // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem});
             this.menuStrip1.Location = new System.Drawing.Point(0, 0);
             this.menuStrip1.Name = "menuStrip1";
             this.menuStrip1.Size = new System.Drawing.Size(784, 24);
             this.menuStrip1.TabIndex = 1;
             this.menuStrip1.Text = "menuStrip1";
             // 
-            // Form1
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.goToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // goToolStripMenuItem
+            // 
+            this.goToolStripMenuItem.Name = "goToolStripMenuItem";
+            this.goToolStripMenuItem.Size = new System.Drawing.Size(89, 22);
+            this.goToolStripMenuItem.Text = "Go";
+            this.goToolStripMenuItem.Click += new System.EventHandler(this.goToolStripMenuItem_Click);
+            // 
+            // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
@@ -58,9 +83,11 @@
             this.Controls.Add(this.canvas);
             this.Controls.Add(this.menuStrip1);
             this.MainMenuStrip = this.menuStrip1;
-            this.Name = "Form1";
+            this.Name = "MainForm";
             this.Text = "Fractals.Net";
             ((System.ComponentModel.ISupportInitialize)(this.canvas)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -70,6 +97,8 @@
 
         private System.Windows.Forms.PictureBox canvas;
         private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem goToolStripMenuItem;
     }
 }
 
--- Src/Fractals.Forms/MainForm.cs
+++ Src/Fractals.Forms/MainForm.cs
@@ -3,8 +3,11 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.Drawing.Imaging;
 using System.Linq;
+using System.Numerics;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -12,9 +15,61 @@ namespace Fractals.Forms
 {
     public partial class MainForm : Form
     {
+        const double rMin = -2.5, rMax = 1.0, iMin = -0.5, iMax = 0.5;
+
+        Color[] pallette;
+
         public MainForm()
         {
             InitializeComponent();
+
+            pallette = new Color[255];
+
+            for (int i = 0; i < 255; i++)
+            {
+                pallette[i] = Color.FromArgb(255, i, i, 255);
+            }
+        }
+
+        private void goToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            ThreadPool.QueueUserWorkItem(new WaitCallback(o => CalculateBrot()));
+        }
+
+        protected override void OnLoad(EventArgs e)
+        {
+            base.OnLoad(e);
+        }
+
+        private void CalculateBrot()
+        {
+            using (var g = canvas.CreateGraphics())
+            {
+                var rScale = (Math.Abs(rMin) + Math.Abs(rMax)) / canvas.Width;
+                var iScale = (Math.Abs(iMin) + Math.Abs(iMax)) / canvas.Height;
+
+                for (int x = 0; x < canvas.Width; x++)
+                {
+                    for (int y = 0; y < canvas.Height; y++)
+                    {
+                        Complex c = new Complex(x * rScale + rMin, y * iScale + iMin);
+                        Complex z = c;
+
+                        for (int i = 0; i < pallette.Length; i++)
+                        {
+                            if (z.Magnitude >= 2.0)
+                            {
+                                g.FillRectangle(new SolidBrush(pallette[i]), x, y, 1, 1);
+                                break;
+                            }
+                            else
+                            {
+                                z = c + Complex.Pow(z, 2);
+                            }
+                        }
+                    }
+                }
+            }
         }
     }
 }
